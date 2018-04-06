915b97ecda7edb0aff5881ce81b8e10c888be6b1
Denise Pendleton <dpendl1@hotmail.com>
2018-04-06 21:21:38 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -4,9 +4,10 @@ class Show < ActiveRecord::Base
   has_many :actors, through: :characters
   belongs_to :networks
 
-  def build_network(attributes)
-    # binding.pry
-    # attributes.each do {|key, value| self.send (networks.key, "value")}
+  # def build_network(:network => )
+  #   # binding.pry
+  #   # attributes.each do {|key, value| self.send (networks.key, "value")}
+  #   self.network = attributes[]
 
   end
 
